9903770bf62cbbaeebb06b5b4db2f2279486fda6
tikkhun <s565338107@163.com>
2023-03-14 23:13:12 +0800

    feat(react): react could usedev server

--- packages/react/config/webpack.dev.config.js
+++ packages/react/config/webpack.dev.config.js
@@ -15,3 +15,23 @@
  * @done
  * @example
  */
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.config.js');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    port: 9000,
+    compress: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html',
+      inject: 'body',
+      hash: false,
+    }),
+  ],
+});
+
--- packages/react/config/webpack.prod.config.js
+++ packages/react/config/webpack.prod.config.js
@@ -18,7 +18,30 @@
 
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.config.js");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    new HtmlWebpackPlugin({
+      // html
+      template: "public/index.html",
+      // 打包后的 html 文件名
+      filename: "index.html",
+      // 将 js 注入 body 最底部
+      inject: "body",
+      // 压缩 html 文件的配置
+      minify: {
+        // 去除注释
+        removeComments: true,
+      },
+    }),
+    // 清空dist
+    new CleanWebpackPlugin(),
+  ],
+  output: {
+    // 随机名称
+    filename: "js/[name]-bundle-[hash:6].js",
+  },
 });
